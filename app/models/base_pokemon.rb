ef461f742d6cbf35bbbbf0ba96bc52af14772ae3
Trevor Nodland <trevor.nodland@gmail.com>
2020-08-21 18:10:10 -0600

    adds base_pokemon model and tests validations

--- app/models/base_pokemon.rb
+++ app/models/base_pokemon.rb
@@ -1,2 +1,7 @@
 class BasePokemon < ApplicationRecord
+    validates_presence_of :name,
+                          :form,
+                          :base_attack,
+                          :base_defense,
+                          :base_stamina
 end
--- db/migrate/20200821205605_create_base_pokemons.rb
+++
@@ -1,8 +0,0 @@
-class CreateBasePokemons < ActiveRecord::Migration[5.2]
-  def change
-    create_table :base_pokemons do |t|
-
-      t.timestamps
-    end
-  end
-end
